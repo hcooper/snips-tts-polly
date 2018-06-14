1a9f0573e607e062ffef1ce128786795f5df15a5
Your Name <you@example.com>
2018-06-13 21:33:03 -0700

    Fixed sayFinished issue & general tidy up

--- snips-tts-polly.py
+++ snips-tts-polly.py
@@ -1,61 +1,55 @@
 #!/usr/bin/env python3
-import sys
-import subprocess
+import hashlib
+import json
+import os
 import random
 import string
+import subprocess
+from pathlib import Path
+
+import boto3
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
-import json
-import boto3
-from pathlib import Path
-import os
-import hashlib
 import toml
 
-'''
-This is meant to be a replacement for snips-tts, using the higher quality
+
+"""
+This is a replacement for `snips-tts`, using the higher quality
 AWS Polly service to produce audio from text.
 
 It listens on the same MQTT topics as snips-tts. Both can run at the same time,
 but it will cause duplicate audio output. Therefore stopping the existing
-snips-tts service is probably best.
+snips-tts service is probably best (`systemctl stop|disable snips-tts`).
 
 Initially developed from jarvis_listener.py by @tschmidty69
 https://github.com/tschmidty69/homeassistant-config/blob/master/shell_command/jarvis_listener.py
-'''
-
-def log(msg):
-    print(msg)
+"""
 
 
 def on_connect(client, userdata, flags, rc):
-    log("MQTT Connected")
+    print("MQTT connected")
     client.subscribe("hermes/tts/say")
-    client.subscribe("hermes/audioServer/default/playFinished")
 
 
-def _hash(text):
+def _hash(text: str) -> str:
     return hashlib.md5(text.encode()).hexdigest()
 
 
-def _random_id():
+def _random_id() -> str:
     return "".join(
         [random.choice(string.ascii_uppercase + string.digits) for i in range(16)]
     )
 
 
-def _convert_mp3_to_wav(mp3_path, wav_path, delete=True):
+def _convert_mp3_to_wav(mp3_path: Path, wav_path: Path, delete: bool = True) -> None:
     """ Uses mpg123 to convert mp3->wav, and delete the original mp3 """
     subprocess.run(["/usr/bin/mpg123", "-q", "-w", str(wav_path), str(mp3_path)])
     if delete:
         os.remove(str(mp3_path))
 
 
-def tts_say(client, userdata, msg, voice="Raveena"):
-    log(msg.topic + " " + str(msg.payload.decode()))
+def tts_say(client, userdata, msg, voice="Raveena") -> None:
     data = json.loads(msg.payload.decode())
-
-    sessionId = _random_id()  # We make a random one
 
     tmp_tts_dir = "/tmp/tts/"
     Path(tmp_tts_dir).mkdir(parents=True, exist_ok=True)
@@ -66,7 +60,7 @@ def tts_say(client, userdata, msg, voice="Raveena"):
 
     response = {}
     if not wav_path.is_file():
-        log("No cached file found, querying polly.")
+        print("Cached file not found, querying polly.")
 
         response = aws_client.synthesize_speech(
             OutputFormat="mp3", Text=data["text"], VoiceId=voice
@@ -76,64 +70,34 @@ def tts_say(client, userdata, msg, voice="Raveena"):
 
         _convert_mp3_to_wav(mp3_path, wav_path)
     else:
-        log("Using cached file: {}".format(wav_path))
+        print("Using cached file: {}".format(wav_path))
 
-    publish.single(
-        "hermes/audioServer/default/playBytes/" + sessionId + "/",
-        payload=wav_path.open("rb").read(),
-        qos=0,
-        retain=False,
-        hostname=mqtt_host,
-        port=mqtt_port,
-        client_id="",
-        keepalive=60,
-        will=None,
-        auth=None,
-        tls=None,
-        protocol=mqtt.MQTTv311,
-    )
+    msgs = [
+        {
+            "topic": "hermes/audioServer/default/playBytes/{}".format(_random_id()),
+            "payload": wav_path.open("rb").read(),
+        },
+        {
+            "topic": "hermes/tts/sayFinished",
+            "payload": json.dumps({"id": data["id"], "sessionId": data["sessionId"]}),
+        },
+    ]
 
-    # If 'data' contrains a sessionId the request came from a snips dialogue
-    # so let it know we are done talking.
-    if "sessionId" in data:
-        publish.single(
-            "hermes/tts/sayFinished",
-            payload='{"siteId": "'
-            + data["siteId"]
-            + '", "sessionId": "'
-            + data["sessionId"]
-            + '", "id": "'
-            + data["id"]
-            + '"}',
-            qos=0,
-            retain=False,
-            hostname=mqtt_host,
-            port=mqtt_port,
-            client_id="",
-            keepalive=60,
-            will=None,
-            auth=None,
-            tls=None,
-            protocol=mqtt.MQTTv311,
-        )
+    publish.multiple(msgs, hostname=mqtt_host, port=mqtt_port)
 
-
-def playFinished(client, userdata, msg):
-    log(msg.topic + " " + str(msg.payload.decode()))
 
 # Read MQTT connection info from the central snips config.
-config = toml.loads(open("/etc/snips.toml").read())
+snips_config = toml.loads(open("/etc/snips.toml").read())
 
 client = mqtt.Client()
 client.on_connect = on_connect
 
 client.message_callback_add("hermes/tts/say", tts_say)
-client.message_callback_add("hermes/audioServer/default/playFinished", playFinished)
 
-mqtt_host, mqtt_port = config["snips-common"]["mqtt"].split(":")
+mqtt_host, mqtt_port = snips_config["snips-common"]["mqtt"].split(":")
 mqtt_port = int(mqtt_port)
 client.connect(mqtt_host, mqtt_port, 60)
 
-aws_client = boto3.client("polly")
+aws_client = boto3.client("polly")  # assuming boto3 is configured
 
 client.loop_forever()
